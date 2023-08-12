e031f1663dd10d021c5072ca2ba25cfafd2cb6e2
Shrikrishna Keeliputti <73592987+Shrikrishnak2210@users.noreply.github.com>
2023-08-12 12:43:11 +0530

    third commit

--- BookMavenJDBC/src/main/java/com/bookapp/dao/BookDaoImpl.java
+++ BookMavenJDBC/src/main/java/com/bookapp/dao/BookDaoImpl.java
@@ -7,6 +7,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import com.bookapp.model.Book;
+import com.bookapp.util.DbConnection;
 import com.bookapp.util.Queries;
 
 public class BookDaoImpl implements IBookDao {
---
+++ BookMavenJDBC/src/main/java/com/bookapp/util/DbConnection.java
@@ -0,0 +1,34 @@
+package com.bookapp.util;
+
+import java.sql.Connection; 
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class DbConnection {
+	static Connection connection;
+
+	public static Connection openConnection() {
+		String url = "jdbc:mysql://localhost:3306/mysql";
+		String username = "root";
+		String password = "admin@123";
+
+		try {
+			connection = DriverManager.getConnection(url, username, password);
+			PreparedStatement preparedStatement = connection.prepareStatement(Queries.CREATEQUERY);
+			preparedStatement.execute();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return connection;
+	}
+
+	public static void closeConnection() {
+		try {
+			if (connection != null)
+				connection.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
